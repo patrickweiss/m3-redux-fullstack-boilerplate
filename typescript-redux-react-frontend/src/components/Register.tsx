bc2a9e24856af7ba81956d0460c569b267667dfc
Patrick Weiss <patrick.weiss@officeone.team>
2019-10-17 16:39:30 +0200

    html for register

--- typescript-redux-react-frontend/src/components/Register.tsx
+++ typescript-redux-react-frontend/src/components/Register.tsx
@@ -1,10 +1,24 @@
 import React, { Component } from 'react'
 
+
 export default class Register extends Component {
     render() {
         return (
             <div>
-                <h1>Register</h1>
+                <form action="">
+                    <label htmlFor="firstname">First name:</label>
+                    <input type="firstname" placeholder="firstname" />
+                    <br />
+                    <label htmlFor="lastname">Last name:</label>
+                    <input type="lastname" placeholder="lastname" />
+                    <br />
+                    <label htmlFor="username">Username:</label>
+                    <input type="username" placeholder="Your username" />
+                    <br />
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" placeholder="********" />
+                    <br />
+                </form>
             </div>
         )
     }
